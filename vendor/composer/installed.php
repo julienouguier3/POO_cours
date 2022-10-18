5ce7d47f8f7503f164dda0f4ea4593c57f07cfeb
Julie Nouguier <nouguier.j3@gmail.com>
2022-10-18 15:32:15 +0200

    Init project Poo

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'julie/zoo',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '1e6778c52ca967f27e946313d22e2be1e9d9bf84',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'julie/zoo' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '1e6778c52ca967f27e946313d22e2be1e9d9bf84',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
